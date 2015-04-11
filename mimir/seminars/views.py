06b2aa5d38a7fabbee31729321c7b5549c9cd693
Akollek <amiel.kollek@mail.mcgill.ca>
2015-04-11 06:00:10 -0400

    basic registration

--- mimir/seminars/views.py
+++ mimir/seminars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect 
-
+from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
@@ -16,10 +16,11 @@ def register(request, error=""):
                 # validate user
                 user = User.objects.filter(username = username)
                 if user.exists():
-                        return render_to_response('registration.html',{ 'error' : 'Username taken!' })
+                        return render_to_response('registration.html',{ 'error' : 'Username taken!'}, context_instance=RequestContext(request))
+
                 user = User.objects.filter(email = email)
                 if user.exists():
-                        return render_to_response('registration.html',{ 'error' : 'Email taken!' })
+                        return render_to_response('registration.html',{ 'error' : 'Email taken!' }, context_instance=RequestContext(request))
                 user = User.objects.create_user(username,email,password)
                 user.save()
 
@@ -27,7 +28,5 @@ def register(request, error=""):
                 login(request,user)
                 return HttpResponseRedirect('index')
         else:   
-                error = request.GET['error']
-                
-                return render_to_response('registration.html',{'error':''})
+                return render_to_response('registration.html',{'error':''}, context_instance=RequestContext(request))
                 
