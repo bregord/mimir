b53013ba71a558676c9767737d3bbaca720369b8
Akollek <amiel.kollek@mail.mcgill.ca>
2015-04-11 11:48:23 -0400

    updated models

--- mimir/forum/models.py
+++ mimir/forum/models.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Seminar(models.Model):
-        author   = models.ForeignKey(User)
-        topic    = models.CharField(max_length=300)
-        title    = models.CharField(max_length=50)
-        contents = models.TextField()
+        author      = models.ForeignKey(User)
+        topic       = models.CharField(max_length=500)
+        title       = models.CharField(max_length=50)
+        contents    = models.TextField()
+        description = models.CharField(max_length=500)
+        date        = models.DateTimeField('date written')
 
 
 class Discussions(models.Model):
--- mimir/registration/models.py
+++ mimir/registration/models.py
@@ -4,5 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class UserProfile(models.Model):
-       user = models.OneToOneField(User)
-       picture = models.ImageField(upload_to='profile_images', blank=True,)
+       user        = models.OneToOneField(User)
+       picture     = models.ImageField(upload_to='profile_images', blank=True,)
+       description = models.CharField(max_length=500,blank=True)
+       interests   = models.CharField(max_length=200,blank=True)
+       website     = models.URLField(max_length=200,blank=True)
+       
+       def __unicode__(self):
+               return self.user.username
