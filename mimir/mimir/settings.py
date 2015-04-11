b827a7aa011cdf485e8f55d7b978d14db7123dad
bregord <sirbrendang@gmail.com>
2015-04-11 08:05:21 -0400

    commit before merge

--- mimir/mimir/settings.py
+++ mimir/mimir/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'registration',
     'django_ace',
+    'django_markdown',
 )
 
-INSTALLED_APPS +=('django_markdown')
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
