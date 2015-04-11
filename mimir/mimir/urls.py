c1fb048e7550ecc55a8a97fe9dfeab72d956415a
Akollek <amiel.kollek@mail.mcgill.ca>
2015-04-11 10:27:30 -0400

    fixed static file serving, added landing page

--- mimir/mimir/urls.py
+++ mimir/mimir/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mimir.views.home', name='home'),
@@ -9,4 +12,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('registration.urls')),
     url(r'^user/', include('user_profiles.urls'))
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
