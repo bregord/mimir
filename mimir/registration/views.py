867fafd08d6b8e8cfa8f6647a3edc081afa95779
Akollek <amiel.kollek@mail.mcgill.ca>
2015-04-11 07:35:48 -0400

    fixed up registration

--- mimir/registration/views.py
+++ mimir/registration/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from registration.models import UserProfile
 
 # Create your views here.
 def index(request):
@@ -16,17 +17,18 @@ def register(request, error=""):
                 # validate user
                 user = User.objects.filter(username = username)
                 if user.exists():
-                        return render_to_response('registration.html',{ 'error' : 'Username taken!'}, context_instance=RequestContext(request))
+                        return render_to_response('registration.html',{ 'error' : 'UserProfilename taken!'}, context_instance=RequestContext(request))
 
                 user = User.objects.filter(email = email)
                 if user.exists():
                         return render_to_response('registration.html',{ 'error' : 'Email taken!' }, context_instance=RequestContext(request))
                 user = User.objects.create_user(username,email,password)
                 user.save()
-
+                userProfile = UserProfile(user=user)
+                userProfile.save()
                 user = authenticate(username=username,password=password)
                 login(request,user)
-                return HttpResponseRedirect('index')
+                return HttpResponseRedirect('/index')
         else:   
                 return render_to_response('registration.html',{'error':''}, context_instance=RequestContext(request))
                 
